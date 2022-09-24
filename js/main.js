4d7d35d64e663d532bc44032ecdd44728831d7f8
Max753 <mkozhen@mail.ru>
2022-09-24 13:00:58 +0300

    ToDo-List

---
+++ js/main.js
@@ -0,0 +1,37 @@
+
+
+const todoList = document.querySelector('#todo-list');
+const todoForm = document.querySelector('#todo-form');
+const todoInput = document.querySelector('#todo-input');
+
+todoForm.addEventListener('submit', formHandler);
+
+
+function formHandler (event) {
+    event.preventDefault();
+
+    const taskText = todoInput.value; 
+    
+    const newTask = document.createElement('li');
+    newTask.innerText = taskText;
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.setAttribute('role', 'button');
+    deleteBtn.innerText = 'Удалить';
+    deleteBtn.style['margin-left'] = '15px';
+
+    newTask.append(deleteBtn);
+
+    deleteBtn.addEventListener('click', deleteTask);
+
+    function deleteTask(){
+        newTask.remove();
+    }
+
+    todoList.append(newTask);
+
+    todoInput.value = '';
+
+    todoInput.focus();
+
+}
